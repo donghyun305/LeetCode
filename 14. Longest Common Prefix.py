9dbbde1b510d642b96ac95c75cca98b15801b0f5
donghyun305 <donghyn305@naver.com>
2022-08-29 16:03:22 +0900

    14

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def longestCommonPrefix(strs: List[str]):
+    strs.sort(key=lambda x : len(x))
+
+    if len(strs)==0 or len(strs)==1:
+        return ""
+    else:
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if strs[0][i] != strs[j][i]:
+                    return strs[0][:i]
+        return strs[0]
+
+print(longestCommonPrefix(['flower', 'fly', 'fldasdas']))
